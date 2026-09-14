46a4bc31a0c01c4e84042c9a8b9a2fe1e9711735
kinfolk812-png <kinfolk812@gmail.com>
2026-09-14 23:27:48 +0530

    Update predictor.py

--- Medicure-Predictor-AI-Assistant/app.py/modules/predictor.py
+++ Medicure-Predictor-AI-Assistant/app.py/modules/predictor.py
@@ -1,86 +1,60 @@
 import json
+from pathlib import Path
 
 
 def load_diseases():
 
+    # Get the app.py folder
+    base_dir = Path(__file__).resolve().parent.parent
+
     files = [
-        "database/diseases.json",
-
-        "database/diseases_extra.json",
-
-        "database/diseases_part3.json",
-
-        "database/diseases_part4.json"
-
+        base_dir / "database" / "diseases.json",
+        base_dir / "database" / "diseases_extra.json",
+        base_dir / "database" / "diseases_part3.json",
+        base_dir / "database" / "diseases_part4.json"
     ]
 
     all_diseases = {}
 
-
     for file in files:
-
-        with open(file,"r") as f:
-
+        with open(file, "r", encoding="utf-8") as f:
             data = json.load(f)
-
             all_diseases.update(data)
-
 
     return all_diseases
 
 
-
 def predict_disease(selected_symptoms):
-
 
     diseases = load_diseases()
 
+    results = []
 
-    results=[]
+    for disease, data in diseases.items():
 
+        score = 0
+        total = 0
 
-    for disease,data in diseases.items():
-
-
-        score=0
-
-        total=0
-
-
-        for symptom,weight in data["Weights"].items():
+        for symptom, weight in data["Weights"].items():
 
             total += weight
 
-
             if symptom in selected_symptoms:
-
                 score += weight
-
-
 
         confidence = 0
 
-
         if total > 0:
-
-            confidence = (score/total)*100
-
-
+            confidence = (score / total) * 100
 
         results.append({
-
-            "Disease":disease,
-
-            "Confidence":round(confidence,2)
-
+            "Disease": disease,
+            "Confidence": round(confidence, 2)
         })
-
-
 
     results.sort(
         key=lambda x: x["Confidence"],
         reverse=True
     )
-
 
     return results[:5]
