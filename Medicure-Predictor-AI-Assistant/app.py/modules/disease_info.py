c64c75ce88bf9ce0516c59eeac7ffbcfb27eded1
kinfolk812-png <kinfolk812@gmail.com>
2026-09-14 23:34:36 +0530

    Update disease_info.py

--- Medicure-Predictor-AI-Assistant/app.py/modules/disease_info.py
+++ Medicure-Predictor-AI-Assistant/app.py/modules/disease_info.py
@@ -1,31 +1,33 @@
 import json
+from pathlib import Path
 
 
 def load_information():
 
+    base_dir = Path(__file__).resolve().parent.parent
+
+    information_file = base_dir / "database" / "disease_information.json"
+
     with open(
-        "database/disease_information.json",
-        "r"
+        information_file,
+        "r",
+        encoding="utf-8"
     ) as file:
 
         return json.load(file)
-
 
 
 def get_disease_info(disease):
 
     data = load_information()
 
-
     if disease in data:
 
         return data[disease]
 
-
     else:
 
         return {
-
             "Description":
             "Information will be updated soon.",
 
@@ -40,5 +42,4 @@ def get_disease_info(disease):
 
             "Warning Signs":
             []
-
         }
